376b5286990c742e55f2e94226da0d0c09813a4d
fabrice.rosito <fabrice.rosito@gmail.com>
2024-02-26 19:52:37 +0100

    feat: bt iphon

--- src/presentation/home.tsx
+++ src/presentation/home.tsx
@@ -1,17 +1,57 @@
 import * as React from 'react';
 import { Trans } from 'react-i18next';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import InputIcon from '@mui/icons-material/Input';
 
 import '@presentation/home.scss';
 import Bar from '@presentation/bar';
 import { Footer } from '@presentation/footer';
 
 export const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <Bar/>
       <div className="home">
         <div className='title'>
           <Trans>home.title</Trans>
+        </div>
+        <div>
+          <Box 
+            sx={{
+              display: 'flex',
+              alignItems: 'flex-center',
+              flexDirection: 'column',
+              p: 1,
+              m: 1,
+            }}
+          >
+            <Button 
+              sx={{
+                m: 1,
+              }}
+              type="submit"
+              variant="contained"
+              size="small"
+              startIcon={<InputIcon />}
+              onClick={(e) => {
+                navigate("/bank");
+              }}
+            ><Trans>home.bank</Trans></Button>
+            <Button 
+              sx={{
+                m: 1,
+              }}
+              type="submit"
+              variant="contained"
+              size="small"
+              startIcon={<InputIcon />}
+              onClick={(e) => {
+                navigate("/password");
+              }}
+            ><Trans>home.password</Trans></Button>
+          </Box>
         </div>
       </div>
       <Footer />
--- src/service/graphql/graphql.service.fake.ts
+++ src/service/graphql/graphql.service.fake.ts
@@ -17,6 +17,37 @@ export class GraphqlServiceFake implements GraphqlService {
           }
         }
       });
+    } else if (datas.operationName === 'getSessionInfo') {
+      return Promise.resolve({
+        "data": {
+          "getSessionInfo": {
+            "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiZmFybyIsImlkIjoiNjVkODliOWVkY2Q4Y2JiYTlhN2NlNjUxIiwiaWF0IjoxNzA4NzYzNTgxLCJleHAiOjE3MDg3OTIzODF9.o4cQ-j1FEX0RgTYRm5R2ivt2770An_b2XHsYgmcgjdA",
+            "id": "65d89b9edcd8cbba9a7ce651",
+            "code": "faro",
+            "name_first": "fabrice",
+            "name_last": "rosito",
+            "description": "description",
+            "mail": "fabrice.rosito@gmail.com",
+            "role": "ADMIN"
+          }
+        }
+      });
+    } else if (datas.operationName === 'systemInfo') {
+      return Promise.resolve({
+        "data": {
+          "systemInfo": {
+            "version": "0.1.0"
+          }
+        }
+      });
+    } else if (datas.operationName === 'generatePassword') {
+      return Promise.resolve({
+        "data": {
+          "generatePassword": {
+            "password": "HaHaHa"
+          }
+        }
+      });
     }
     throw new Error('Method not implemented.');
   }
