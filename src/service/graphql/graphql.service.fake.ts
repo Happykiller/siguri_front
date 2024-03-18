a75e745161bc2443ee42d732b443c0b51aacf0b7
happykiller <fabrice.rosito@gmail.com>
2024-03-18 19:01:02 +0100

    feat: complete fake

--- src/service/graphql/graphql.service.fake.ts
+++ src/service/graphql/graphql.service.fake.ts
@@ -6,9 +6,9 @@ export class GraphqlServiceFake implements GraphqlService {
       return Promise.resolve({
         "data": {
           "auth": {
-            "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiZmFybyIsImlkIjoiNjVkODliOWVkY2Q4Y2JiYTlhN2NlNjUxIiwiaWF0IjoxNzA4NzYzNTgxLCJleHAiOjE3MDg3OTIzODF9.o4cQ-j1FEX0RgTYRm5R2ivt2770An_b2XHsYgmcgjdA",
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiZmFybyIsImlkIjoiNjVkODliOWVkY2Q4Y2JiYTlhN2NlNjUxIiwiaWF0IjoxNzA4NzYzNTgxLCJleHAiOjE3MDg3OTIzODF9.o4cQ-j1FEX0RgTYRm5R2ivt2770An_b2XHsYgmcgjdA",
             "id": "65d89b9edcd8cbba9a7ce651",
-            "code": "faro",
+            "code": "user1",
             "name_first": "fabrice",
             "name_last": "rosito",
             "description": "description",
@@ -21,9 +21,9 @@ export class GraphqlServiceFake implements GraphqlService {
       return Promise.resolve({
         "data": {
           "getSessionInfo": {
-            "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiZmFybyIsImlkIjoiNjVkODliOWVkY2Q4Y2JiYTlhN2NlNjUxIiwiaWF0IjoxNzA4NzYzNTgxLCJleHAiOjE3MDg3OTIzODF9.o4cQ-j1FEX0RgTYRm5R2ivt2770An_b2XHsYgmcgjdA",
+            "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiZmFybyIsImlkIjoiNjVkODliOWVkY2Q4Y2JiYTlhN2NlNjUxIiwiaWF0IjoxNzA4NzYzNTgxLCJleHAiOjE3MDg3OTIzODF9.o4cQ-j1FEX0RgTYRm5R2ivt2770An_b2XHsYgmcgjdA",
             "id": "65d89b9edcd8cbba9a7ce651",
-            "code": "faro",
+            "code": "user1",
             "name_first": "fabrice",
             "name_last": "rosito",
             "description": "description",
@@ -48,7 +48,69 @@ export class GraphqlServiceFake implements GraphqlService {
           }
         }
       });
+    } else if (datas.operationName === 'chestsForUser') {
+      return Promise.resolve({
+        "data": {
+          "chestsForUser": [
+            {
+              "id": "65e0bb411a54812572091e49",
+              "label": "Coffre",
+              "description": "Coffre de test",
+              "author_id": "65da139c45e382cdb661379f",
+              "author": {
+                "id": "65da139c45e382cdb661379f",
+                "code": "user1"
+              },
+              "members": [
+                {
+                  "user_id": "65da139c45e382cdb661379f",
+                  "user": {
+                    "id": "65da139c45e382cdb661379f",
+                    "code": "user1"
+                  }
+                },
+                {
+                  "user_id": "65da139c45e382cdb661379g",
+                  "user": {
+                    "id": "65da139c45e382cdb661379g",
+                    "code": "user2"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      });
+    } else if (datas.operationName === 'thingsForChest') {
+      return Promise.resolve({
+        "data": {
+          "thingsForChest": [
+            {
+              "id": "65e0bc221a54812572091e53",
+              "label": "Test",
+              "description": "Test",
+              "author": {
+                "id": "65da139c45e382cdb661379f",
+                "code": "user1"
+              },
+              "chest": {
+                "id": "65e0bb411a54812572091e49",
+                "label": "Coffre"
+              },
+              "type": "CODE",
+              "cb": null,
+              "code": {
+                "code": "clé 4242 clé"
+              },
+              "credential": null,
+              "note": null,
+              "totp": null
+            }
+          ]
+        }
+      });
     }
+
     throw new Error('Method not implemented.');
   }
 } 
